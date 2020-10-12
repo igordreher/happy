efc8b13eeca10db504d0fd442b4ab3b54d4fa609
igordreher <igord.sparrenberger@gmail.com>
2020-10-12 19:54:45 -0300

    Added orphanages-map page's content

--- src/pages/Orphanages-Map.tsx
+++ src/pages/Orphanages-Map.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-function OrphanagesMap(){
-    return(
-        <h1>Hello World</h1>
+import '../styles/pages/orphanages-map.css'
+import mapMarkerImg from '../images/map_marker.svg';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Logo" />
+                    <h2>
+                        Escolha um orfanato no mapa
+                    </h2>
+                    <p>
+                        Muitas crianças estão esperando a sua visita!
+                    </p>
+                </header>
+                <footer>
+                    <strong>Brasília</strong>
+                    <span>Distrito Federal</span>
+                </footer>
+            </aside>
+
+            <div></div>
+        </div>
     );
 }
 
-export default OrphanagesMap
+export default OrphanagesMap;
