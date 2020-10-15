aa67d3906f23af54ea1ffa6e22e130ccdb262f7b
igordreher <igord.sparrenberger@gmail.com>
2020-10-15 18:12:19 -0300

    Fix: equal sign on orphanage page

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -57,7 +57,7 @@ export default function Orphanage() {
               return (
                 <button
                   key={image.id}
-                  className={activeImageIndex == index ? 'active' : ''}
+                  className={activeImageIndex === index ? 'active' : ''}
                   type="button"
                   onClick={() => {
                     setActiveImageIndex(index)
