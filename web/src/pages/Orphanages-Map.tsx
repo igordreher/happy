e589ccc716b154fbefb706f27c8de90281579ef2
igordreher <igord.sparrenberger@gmail.com>
2020-10-15 13:20:55 -0300

    Update: abstracted mapIcon to utils

--- web/src/pages/Orphanages-Map.tsx
+++ web/src/pages/Orphanages-Map.tsx
@@ -2,20 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
-import Leaflet from 'leaflet';
 
-import 'leaflet/dist/leaflet.css'
+import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/map-icon'
 
 import '../styles/pages/orphanages-map.css';
-import mapMarkerImg from '../images/map-marker.svg';
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [200, 2]
-})
-
 
 function OrphanagesMap() {
     return (
@@ -39,19 +30,19 @@ function OrphanagesMap() {
             </aside>
 
             <Map
-                center={[-15.825297,-47.9372209]}
+                center={[-15.825297, -47.9372209]}
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                <Marker 
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker
                     icon={mapIcon}
-                    position={[-15.825297,-47.9372209]}
+                    position={[-15.825297, -47.9372209]}
                 >
                     <Popup closeButton={false} maxWidth={240} minWidth={240} className='map-popup'>
                         Lar das meninas
                         <Link to="/orphanages/1">
-                            <FiArrowRight size={20} color="#fff"/>
+                            <FiArrowRight size={20} color="#fff" />
                         </Link>
                     </Popup>
                 </Marker>
